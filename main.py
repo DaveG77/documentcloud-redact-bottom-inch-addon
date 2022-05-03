c487a587454b59716444939d26917700d30baa3d
Dave Godfrey <rocketdave2564@gmail.com>
2022-05-03 12:45:34 -0400

    formatted with black

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ DocumentCloud using the standard API
 from listcrunch import uncrunch
 from documentcloud.addon import AddOn
 
+
 class RedactInch(AddOn):
     def main(self):
         if not self.documents:
@@ -17,19 +18,23 @@ class RedactInch(AddOn):
         self.set_message("Redact Bottom Inch start!")
 
         for document in self.client.documents.list(id__in=self.documents):
-            #go through each page
+            # go through each page
             pages = document.page_count
 
-            #to hold the redacttion json dictionary for each individual page in this document
+            # to hold the redacttion json dictionary for each individual page in this document
             eachPage = []
 
             for page in range(pages):
-                #append the specific json dict for this page to global dict
-                eachPage.append({"page_number": page, "x1": 0, "y1": 0.9, "x2": 1, "y2": 1})
+                # append the specific json dict for this page to global dict
+                eachPage.append(
+                    {"page_number": page, "x1": 0, "y1": 0.9, "x2": 1, "y2": 1}
+                )
 
-            #make the api call for this document
+            # make the api call for this document
             self.client.post(f"documents/{document.id}/redactions/", json=eachPage)
 
         self.set_message("Redact Bottom Inch end!")
+
+
 if __name__ == "__main__":
     RedactInch().main()
